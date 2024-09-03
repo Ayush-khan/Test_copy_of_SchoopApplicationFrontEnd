5cde4faaa466042e2bc2ed6a272e7e725d806346
Ayush-Khan <ayushkhana230@gmail.com>
2024-09-03 06:52:53 -0700

    .

--- src/componants/StudentModel/ManageStudent.jsx
+++ src/componants/StudentModel/ManageStudent.jsx
@@ -159,7 +159,7 @@ function ManageSubjectList() {
     fetchClassNamesForAllotSubject();
   }, []);
 
-  // Example usage within useEffect fg fgd
+  // Example usage within useEffect fg fgd rtdhg hfbrdh
 
   const fetchClassNamesForAllotSubject = async () => {
     try {
