e3f406709d091a7e467240e67749f3895d29713d
Ayush-Khan <ayushkhana230@gmail.com>
2024-09-11 06:30:50 -0700

    .

--- src/componants/StudentModel/CreateStudent.jsx
+++ src/componants/StudentModel/CreateStudent.jsx
@@ -1,4 +1,4 @@
-// df  fduierfyhruief
+// df gfhrth dsaawdswe
 // import React, { useState, useEffect } from "react";
 // import { FaUserCircle } from "react-icons/fa";
 // import { RxCross1 } from "react-icons/rx";
