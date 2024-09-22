b8c358eff7f8c6625823a109da9e70770fc0b245
Ayush-Khan <ayushkhana230@gmail.com>
2024-09-22 04:07:31 -0700

    .

--- src/componants/StudentModel/NewStudentList.jsx
+++ src/componants/StudentModel/NewStudentList.jsx
@@ -367,7 +367,7 @@ function NewStudentList() {
           }}
         ></div>
         {/* <hr className="relative -top-3" /> */}
-        {/* gf */}
+        {/* gf  ghrthtr*/}
 
         <div className="bg-white w-full md:w-[95%] mx-auto rounded-md ">
           <div className="w-full  mx-auto">
