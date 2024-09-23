60426eec33cc3c0dfcdbcbafbe381a04b7244169
Ayush-Khan <ayushkhana230@gmail.com>
2024-09-23 04:42:47 -0700

    .

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -372,7 +372,7 @@ const Footer = () => {
             {/* <button
               onClick={handleCloseModal}
               className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md"
-              // jklkj gr
+              // jklkj
             >
               Close
             </button> */}
