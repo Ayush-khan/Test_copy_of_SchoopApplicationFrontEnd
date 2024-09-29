6d21f3ed3124265096d211ed782988683fa550b4
Ayush-Khan <ayushkhana230@gmail.com>
2024-09-29 02:11:08 -0700

    .

--- src/componants/StudentModel/NewStudentList.jsx
+++ src/componants/StudentModel/NewStudentList.jsx
@@ -309,7 +309,7 @@ function NewStudentList() {
     link.click();
     document.body.removeChild(link); // Cleanup after download
   };
-  // gfdg
+  // gfdg fgd
   // Handle file selection
   const handleFileChange = (event) => {
     const file = event.target.files[0];
