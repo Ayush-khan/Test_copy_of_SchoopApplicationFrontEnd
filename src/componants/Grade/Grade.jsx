1875e0fa1954049ed73f7674a06a70ae8f656f53
Ayush-Khan <ayushkhana230@gmail.com>
2024-10-07 06:06:47 -0700

    .

--- src/componants/Grade/Grade.jsx
+++ src/componants/Grade/Grade.jsx
@@ -277,7 +277,7 @@ function Grade() {
     }
   };
 
-  //   fiosd klfgjd
+  //   fiosd
   const handleDelete = (id) => {
     const sectionToDelete = sections.find((sec) => sec.section_id === id);
     setCurrentSection(sectionToDelete);
