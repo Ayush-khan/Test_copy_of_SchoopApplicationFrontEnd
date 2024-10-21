f8753bd8a72f7b221bbe8069999a2ab49168dfb6
Ayush-Khan <ayushkhana230@gmail.com>
2024-10-21 05:02:42 -0700

    .

--- src/componants/Grade/Grade.jsx
+++ src/componants/Grade/Grade.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { RxCross1 } from "react-icons/rx";
 
-// The is the divisionlist module dd nbjkihio
+// The is the divisionlist module dd
 function Grade() {
   const API_URL = import.meta.env.VITE_API_URL; // URL for host
   const [sections, setSections] = useState([]);
