2ac7c0f3593acb56589d826a14f3efba5c42c074
Ayush-Khan <ayushkhana230@gmail.com>
2024-10-22 03:41:04 -0700

    .

--- src/componants/AllotMarkHeading/AllotMarksHeadingTab.jsx
+++ src/componants/AllotMarkHeading/AllotMarksHeadingTab.jsx
@@ -987,7 +987,7 @@ const AllotMarksHeadingTab = () => {
               </div>
             </div>
 
-            {/* Save Button  is that fed*/}
+            {/* Save Button  */}
             <div className="flex justify-center mt-6">
               <button
                 onClick={handleSave}
