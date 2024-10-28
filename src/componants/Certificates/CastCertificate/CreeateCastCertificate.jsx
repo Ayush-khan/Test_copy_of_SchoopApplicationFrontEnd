eaf22a4e93f2493ea80b8056eb98a9aed0a7c6ce
Ayush-Khan <ayushkhana230@gmail.com>
2024-10-28 05:51:34 -0700

    .

--- src/componants/Certificates/CastCertificate/CreeateCastCertificate.jsx
+++ src/componants/Certificates/CastCertificate/CreeateCastCertificate.jsx
@@ -956,7 +956,7 @@ const CreeateCastCertificate = () => {
             </form>
           </div>
         </div>
-        {/* dfs  fdgd fdg*/}
+        {/* dfs */}
         {/* )} */}
       </div>
     </div>
