5f2757a6bf3f255e1d835b81602456241db516db
Ayush-Khan <ayushkhana230@gmail.com>
2024-12-05 07:19:35 -0800

    .

--- src/componants/MastersModule/ExamTimeTable/CreateExamTimeTable.jsx
+++ src/componants/MastersModule/ExamTimeTable/CreateExamTimeTable.jsx
@@ -199,7 +199,7 @@ const CreateExamTimeTable = () => {
       setLoadingForSearch(false);
     }
   };
-  // Function to reset the table
+  // Function to reset the table gfh
   const resetTimetable = () => {
     setTimetable(
       timetable.map((row) => ({
