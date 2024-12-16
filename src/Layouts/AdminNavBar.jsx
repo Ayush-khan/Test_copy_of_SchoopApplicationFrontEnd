1ddb9d881b99357aee00675979e4cc39083b5639
Ayush-Khan <ayushkhana230@gmail.com>
2024-12-16 05:35:46 -0800

    Resove the issue in the 1. Caretaker issue employee ID resolve.... 2. Edit exam time table issue resolved warning alter message logic. 3. Staff employee ID bug fixes. 4. Fix bug of edit class teacher allotment module where teacher name is not come when we edit... in the react select. 5. Download Bala issue bhi solve in the bulk uploading

--- src/Layouts/AdminNavBar.jsx
+++ src/Layouts/AdminNavBar.jsx
@@ -661,6 +661,7 @@ const AdminNavBar = () => {
           Allot Department Coordinator
         </NavDropdown.Item>
       </NavDropdown>
+
       {/* ID Card Dropdown */}
       <NavDropdown
         title={<span className="nav-dropdown-title">ID Card</span>}
