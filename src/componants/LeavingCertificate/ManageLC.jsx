e44aca4551a1b0923db7ad6febd1658700fee31b
Ayush-Khan <ayushkhana230@gmail.com>
2025-01-10 03:56:36 -0800

    resolve the bugs of the leaving certificate searching

--- src/componants/LeavingCertificate/ManageLC.jsx
+++ src/componants/LeavingCertificate/ManageLC.jsx
@@ -68,13 +68,13 @@ function ManageLC() {
   const classOptions = classes.map((cls) => ({
     // value: `${cls?.get_class?.name}-${cls.name}`,
     value: `${cls.class_id}`,
-    label: `${cls?.get_class?.name} ${cls.name}`,
+    label: `${cls?.name}`,
   }));
 
   const fetchClassNames = async () => {
     try {
       const token = localStorage.getItem("authToken");
-      const response = await axios.get(`${API_URL}/api/get_class_section`, {
+      const response = await axios.get(`${API_URL}/api/getClassList`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       if (Array.isArray(response.data)) {
