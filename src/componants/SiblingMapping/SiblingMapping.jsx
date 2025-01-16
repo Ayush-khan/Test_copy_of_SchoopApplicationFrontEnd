37ceff47d834d4150d237a15decf4bc90f8c23b1
Ayush-Khan <ayushkhana230@gmail.com>
2025-01-15 23:15:58 -0800

    add button in the sibling maaping

--- src/componants/SiblingMapping/SiblingMapping.jsx
+++ src/componants/SiblingMapping/SiblingMapping.jsx
@@ -1099,6 +1099,45 @@ const SiblingMapping = () => {
               )}
             </div>
           </div>
+          <div className="w-[98%] mx-auto mb-2  text-right">
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              style={{ backgroundColor: "#2196F3" }}
+              className={`text-white font-bold py-1 border-1 border-blue-500 px-4 rounded ${
+                loading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="flex items-center">
+                  <svg
+                    className="animate-spin h-4 w-4 mr-2 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                    ></path>
+                  </svg>
+                  Loading...
+                </span>
+              ) : (
+                "Save"
+              )}
+            </button>
+          </div>
         </div>
       </div>
     </div>
