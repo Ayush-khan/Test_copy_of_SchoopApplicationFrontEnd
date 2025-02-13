08e7568ff9d5d9ec44d9c3af2eaa57c1ee36aeed
Ayush-Khan <ayushkhana230@gmail.com>
2025-02-13 06:04:43 -0800

    End of the Dummy is....

--- src/componants/NoticeAndSms/NoticeAndSms.jsx
+++ src/componants/NoticeAndSms/NoticeAndSms.jsx
@@ -1088,7 +1088,7 @@ function NoticeAndSms() {
                           const textBeforeCursor = noticeDesc.slice(
                             0,
                             cursorPos
-                          ); // Text before the sdf cursor is:
+                          ); // Text before the cursor is:
 
                           const textAfterCursor = noticeDesc.slice(cursorPos); // Text after the cursor
                           const updatedText = `${textBeforeCursor}\n• ${textAfterCursor}`;
