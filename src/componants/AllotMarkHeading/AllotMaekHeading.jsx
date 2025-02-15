686104abefa879e9c7ab07464920c09a0116170c
Ayush-Khan <ayushkhana230@gmail.com>
2025-02-14 23:54:03 -0800

    Make the Delte api in the ALlot markheading module and added delteed button there

--- src/componants/AllotMarkHeading/AllotMaekHeading.jsx
+++ src/componants/AllotMarkHeading/AllotMaekHeading.jsx
@@ -486,9 +486,9 @@ function AllotMarksHeading() {
                               <th className="px-2 w-full md:w-[8%] text-center lg:px-3 py-2 border border-gray-950 text-sm font-semibold text-gray-900 tracking-wider">
                                 Edit
                               </th>
-                              <th className="px-2 w-full md:w-[8%] text-center lg:px-3 py-2 border border-gray-950 text-sm font-semibold text-gray-900 tracking-wider">
+                              {/* <th className="px-2 w-full md:w-[8%] text-center lg:px-3 py-2 border border-gray-950 text-sm font-semibold text-gray-900 tracking-wider">
                                 Delete
-                              </th>
+                              </th> */}
                             </tr>
                           </thead>
                           <tbody>
@@ -526,7 +526,7 @@ function AllotMarksHeading() {
                                       <FontAwesomeIcon icon={faEdit} />
                                     </button>
                                   </td>
-                                  <td className="px-2 text-center lg:px-3 py-2 border border-gray-950 text-sm">
+                                  {/* <td className="px-2 text-center lg:px-3 py-2 border border-gray-950 text-sm">
                                     <button
                                       onClick={() =>
                                         handleDelete(
@@ -537,7 +537,7 @@ function AllotMarksHeading() {
                                     >
                                       <FontAwesomeIcon icon={faTrash} />
                                     </button>
-                                  </td>
+                                  </td> */}
                                 </tr>
                               ))
                             ) : (
