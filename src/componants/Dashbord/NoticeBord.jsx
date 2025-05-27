d2e3dbceafffbfce7443363f243f388803236c47
Ayush-Khan <ayushkhana230@gmail.com>
2025-05-27 06:42:44 -0700

    push bugish code on notiveboard and c of bugish free code

--- src/componants/Dashbord/NoticeBord.jsx
+++ src/componants/Dashbord/NoticeBord.jsx
@@ -1,3 +1,206 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Styles from "../../CSS/DashbordCss/NoticeBord.module.css";
+
+// function NoticeBord() {
+//   const API_URL = import.meta.env.VITE_API_URL; // url for host
+//   const [activeTab, setActiveTab] = useState("noticeForParents");
+//   const [parentNotices, setParentNotices] = useState([]);
+//   const [staffNotices, setStaffNotices] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchNotices = async () => {
+//       try {
+//         const token = localStorage.getItem("authToken");
+//         const academicYr = localStorage.getItem("academicYear");
+//         if (!token) {
+//           throw new Error("No authentication token  found");
+//         }
+
+//         // Fetch parent notices
+//         const parentResponse = await axios.get(
+//           `${API_URL}/api/parent-notices`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//               "X-Academic-Year": academicYr,
+//             },
+//           }
+//         );
+//         setParentNotices(parentResponse.data.parent_notices);
+
+//         // Fetch staff notices
+//         const staffResponse = await axios.get(`${API_URL}/api/staff-notices`, {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//             "X-Academic-Year": "2023-2024",
+//           },
+//         });
+//         setStaffNotices(staffResponse.data.notices);
+//       } catch (error) {
+//         setError(error.message);
+//         console.error("Error fetching notices:", error);
+//       }
+//     };
+
+//     fetchNotices();
+//   }, []);
+
+//   const handleTabChange = (tab) => {
+//     setActiveTab(tab);
+//   };
+
+//   return (
+//     <div
+//       className={`${Styles.container} bg-slate-100 px-2 rounded-lg shadow-md `}
+//     >
+//       <div
+//         className={` flex justify-between sm:mb-2  w-full  sm:flex flex-row mb-2`}
+//       >
+//         <button
+//           // style={{ width: "100%", height: "3em", fontSize: ".8em" }}
+//           className={`${Styles.tab} w-full h-1/4 text-sm font-bold  ${
+//             activeTab === "noticeForParents" ? Styles.active : ""
+//           } sm:mr-1 mx-0 sm:mb-0  p-2  text-sm sm:w-1/2`}
+//           onClick={() => handleTabChange("noticeForParents")}
+//         >
+//           Notice for Parents
+//         </button>
+//         <button
+//           // style={{ width: "100%", height: "3em", fontSize: ".8em" }}
+//           className={`${Styles.tab} w-full h-1/4 text-sm font-bold  ${
+//             activeTab === "noticeForStaff" ? Styles.active : ""
+//           } sm:mr-1 mx-0 sm:mb-0  p-2 text-sm sm:w-1/2`}
+//           onClick={() => handleTabChange("noticeForStaff")}
+//         >
+//           Notice for Staff
+//         </button>
+//       </div>
+
+//       <div className="overflow-y-auto max-h-64">
+//         {/* For Parent Notices */}
+//         {activeTab === "noticeForParents" && parentNotices.length === 0 && (
+//           <div className="relative left-[1%] w-[95%] text-center flex justify-center items-center mt-8 md:mt-14">
+//             <div className="flex flex-col items-center justify-center text-center ">
+//               <p className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-red-400 to-pink-500 drop-shadow-md mb-3">
+//                 Oops!{" "}
+//               </p>
+
+//               <p className="text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500">
+//                 No data available.
+//               </p>
+//             </div>
+//           </div>
+//         )}
+//         {activeTab === "noticeForParents" && parentNotices.length > 0 && (
+//           <div
+//             className={`${Styles.noticeBoard} grid gap-2 border-4 box-border `}
+//           >
+//             {parentNotices.map((notice, index) => (
+//               <div
+//                 key={index}
+//                 className={`w-full md:w-[100%] border border-gray-300 sm:px-3 sm:py-2 bg-white rounded shadow-md overflow-hidden`}
+//               >
+//                 {/* Date and Type */}
+//                 <div className="text-xs mb-1 flex justify-between items-center text-gray-700">
+//                   <span>{notice.notice_date}</span>
+//                   <span className="font-bold text-blue-600">
+//                     {notice.notice_type}
+//                   </span>
+//                 </div>
+
+//                 {/* Subject and Classes */}
+//                 <div className="text-sm mb-1 text-gray-800">
+//                   <div
+//                     // className="font-medium"
+//                     className={`${Styles.author} text-sm `}
+//                   >
+//                     {notice.subject}
+//                   </div>
+//                   <div
+//                     className={` text-xs sm:mb-1 font-bold mt-1 overflow-x-auto whitespace-wrap max-w-full text-blue-600`}
+//                     style={{
+//                       wordBreak: "break-word",
+//                       overflowWrap: "break-word", // better wrap support
+//                       whiteSpace: "normal", // allow wrapping instead of nowrap
+//                       maxHeight: "100px", // max height if you want vertical scroll
+//                       // maxWidth: "100%",
+//                       // scrollbarWidth: "thin", // For Firefox
+//                       // scrollbarColor: "pink transparent", // For Firefox
+//                       // maxHeight: "100px",
+//                       // overflowX: "auto",
+//                     }}
+//                     // style={{ scrollbarWidth: "thin" }}
+//                   >
+//                     {`( classes-${notice.class_name} )`}
+//                   </div>
+//                 </div>
+
+//                 {/* Description */}
+//                 <div
+//                   className="text-sm leading-4 max-h-[80px] overflow-y-auto break-words text-gray-700"
+//                   style={{ wordBreak: "break-word", maxWidth: "100%" }}
+//                 >
+//                   {notice.notice_desc}
+//                 </div>
+//               </div>
+//             ))}
+//           </div>
+//         )}
+
+//         {/* For Staff Notices */}
+//         {activeTab === "noticeForStaff" && staffNotices.length === 0 && (
+//           <div className="relative left-[1%] w-[95%] text-center flex justify-center items-center mt-8 md:mt-14">
+//             <div className="flex flex-col items-center justify-center text-center ">
+//               <p className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-red-400 to-pink-500 drop-shadow-md mb-3">
+//                 Oops!{" "}
+//               </p>
+
+//               <p className="text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500">
+//                 No data available.
+//               </p>
+//             </div>
+//           </div>
+//         )}
+//         {activeTab === "noticeForStaff" && staffNotices.length > 0 && (
+//           <div className={`${Styles.noticeBoard} grid gap-2`}>
+//             {staffNotices.map((notice, index) => (
+//               <div
+//                 key={index}
+//                 className={`${Styles.notice} sm:p-4 rounded shadow-md`}
+//               >
+//                 <div className={`${Styles.date} text-xs mb-2`}>
+//                   {notice.notice_date}
+//                   <span className={`${Styles.time} float-right font-bold`}>
+//                     {notice.notice_type}
+//                   </span>
+//                 </div>
+//                 <div className={`${Styles.author} text-sm mb-2`}>
+//                   {notice.subject}
+//                   <span
+//                     className={`${Styles.time} ml-2 text-xs`}
+//                   >{`( ${notice.staff_name} )`}</span>
+//                 </div>
+
+//                 <div
+//                   className="text-sm leading-4 max-h-[80px] overflow-y-auto break-words text-gray-700"
+//                   style={{ wordBreak: "break-word", maxWidth: "100%" }}
+//                 >
+//                   {notice.notice_desc}
+//                 </div>
+//                 {/* <div className={`${Styles.message} text-sm leading-6`}></div> */}
+//               </div>
+//             ))}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default NoticeBord;
+// Try UP
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Styles from "../../CSS/DashbordCss/NoticeBord.module.css";
@@ -94,53 +297,26 @@ function NoticeBord() {
           </div>
         )}
         {activeTab === "noticeForParents" && parentNotices.length > 0 && (
-          <div
-            className={`${Styles.noticeBoard} grid gap-2 border-4 box-border `}
-          >
+          <div className={`${Styles.noticeBoard} grid gap-2`}>
             {parentNotices.map((notice, index) => (
               <div
                 key={index}
-                className={`w-full md:w-[100%] border border-gray-300 sm:px-3 sm:py-2 bg-white rounded shadow-md overflow-hidden`}
+                className={`${Styles.notice} sm:border-1 border-gray sm:px-3 sm:py-1 sm:leading-3 mb-0 sm:h-fit bg-white box-border rounded shadow-md `}
               >
-                {/* Date and Type */}
-                <div className="text-xs mb-1 flex justify-between items-center text-gray-700">
-                  <span>{notice.notice_date}</span>
-                  <span className="font-bold text-blue-600">
+                <div className={`${Styles.date} text-xs mb-2 sm:mb-1`}>
+                  {notice.notice_date}
+                  <span className={`${Styles.time} float-right font-bold`}>
                     {notice.notice_type}
                   </span>
                 </div>
-
-                {/* Subject and Classes */}
-                <div className="text-sm mb-1 text-gray-800">
-                  <div
-                    // className="font-medium"
-                    className={`${Styles.author} text-sm `}
-                  >
-                    {notice.subject}
-                  </div>
-                  <div
-                    className={` text-xs sm:mb-1 font-bold mt-1 overflow-x-auto whitespace-wrap max-w-full text-blue-600`}
-                    style={{
-                      wordBreak: "break-word",
-                      overflowWrap: "break-word", // better wrap support
-                      whiteSpace: "normal", // allow wrapping instead of nowrap
-                      maxHeight: "100px", // max height if you want vertical scroll
-                      // maxWidth: "100%",
-                      // scrollbarWidth: "thin", // For Firefox
-                      // scrollbarColor: "pink transparent", // For Firefox
-                      // maxHeight: "100px",
-                      // overflowX: "auto",
-                    }}
-                    // style={{ scrollbarWidth: "thin" }}
-                  >
-                    {`( classes-${notice.class_name} )`}
-                  </div>
+                <div className={`${Styles.author} text-sm mb-2 sm:mb-1`}>
+                  {notice.subject}
+                  <span
+                    className={`${Styles.time} ml-2 text-xs sm:mb-1`}
+                  >{`( classes-${notice.class_name} )`}</span>
                 </div>
-
-                {/* Description */}
                 <div
-                  className="text-sm leading-4 max-h-[80px] overflow-y-auto break-words text-gray-700"
-                  style={{ wordBreak: "break-word", maxWidth: "100%" }}
+                  className={`${Styles.message} text-sm leading-4 sm:leading-3 sm:mt-0`}
                 >
                   {notice.notice_desc}
                 </div>
