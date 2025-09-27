0359b3d19844a85fa8b549e85609b0fb5a3ec85a
Ayush-Khan <ayushkhana230@gmail.com>
2025-09-26 23:52:39 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,6 @@ export default defineConfig({
   build: {
     target: "es2015", // Change this if needed, try "es2015" if esnext doesn't work
     minify: "terser", // Helps avoid modern syntax issues
+    sourcemap: true, // <-- ADD THIS
   },
 });
