20cb2718baa925c1849d8c2aaac39f50579a83f4
Ayush-Khan <ayushkhana230@gmail.com>
2026-01-30 11:34:54 -0800

    Ye  hume paya ka a laaprrperr sepration ho gyaa teacher admn admin ka

--- src/componants/Dashbord/AdminDashboard.jsx
+++ src/componants/Dashbord/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import DashboardContent from "./DashboardContent.jsx";
+// import DashboardContent from "./DashboardContent.jsx";
 import DashboardContainerTry from "./ProperDashbord/DashboardContentTry";
 
 const AdminDashboard = () => {
@@ -15,65 +15,3 @@ const AdminDashboard = () => {
   );
 };
 export default AdminDashboard;
-
-// // test for bg chnage
-// import { useEffect } from "react";
-// import axios from "axios";
-// import DashboardContent from "./DashboardContent.jsx";
-
-// const AdminDashboard = () => {
-//   const API_URL = import.meta.env.VITE_API_URL;
-
-//   const getCookie = (name) => {
-//     const cookieValue = document.cookie
-//       .split("; ")
-//       .find((row) => row.startsWith(name + "="));
-//     return cookieValue ? decodeURIComponent(cookieValue.split("=")[1]) : null;
-//   };
-
-//   useEffect(() => {
-//     const fetchAndSetBackground = async () => {
-//       const token = localStorage.getItem("authToken");
-//       const sortName = getCookie("short_name");
-
-//       if (!token || !sortName) return;
-
-//       try {
-//         const config = {
-//           headers: { Authorization: `Bearer ${token}` },
-//           withCredentials: true,
-//           params: { short_name: sortName },
-//         };
-
-//         const res = await axios.get(
-//           `${API_URL}/api/get_activebackgroundcolor`,
-//           config
-//         );
-
-//         const active = res?.data?.data?.[0];
-
-//         if (active?.color_code && typeof active.color_code === "string") {
-//           // ✅ Save background color to cookie
-//           document.cookie = `bg_color=${encodeURIComponent(
-//             active.color_code
-//           )}; path=/; max-age=86400`; // 1 day
-//         }
-//       } catch (err) {
-//         console.error(
-//           "Failed to fetch background color in dashboard:",
-//           err.message
-//         );
-//       }
-//     };
-
-//     fetchAndSetBackground();
-//   }, []);
-
-//   return (
-//     <div className="w-screen overflow-x-hidden h-screen">
-//       <DashboardContent />
-//     </div>
-//   );
-// };
-
-// export default AdminDashboard;
