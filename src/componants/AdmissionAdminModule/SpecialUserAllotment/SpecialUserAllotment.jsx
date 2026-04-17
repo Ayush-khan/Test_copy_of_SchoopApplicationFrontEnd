0648bcde91ea1f949ce6daed5b0633805aa162fa
CHAUDHARI MAHIMA SURYAKANT <99661594+Mahima-2001@users.noreply.github.com>
2026-04-17 17:54:44 +0530

    phone no -> mobil eno

--- src/componants/AdmissionAdminModule/SpecialUserAllotment/SpecialUserAllotment.jsx
+++ src/componants/AdmissionAdminModule/SpecialUserAllotment/SpecialUserAllotment.jsx
@@ -346,7 +346,7 @@ function SpecialUserAllotment() {
                           Email
                         </th>
                         <th className="px-2  text-center lg:px-3 py-2 border border-gray-950 text-sm font-semibold text-gray-900 tracking-wider">
-                          Phone No.
+                          Mobile No.
                         </th>
 
                         <th className="px-2  text-center lg:px-3 py-2 border border-gray-950 text-sm font-semibold text-gray-900 tracking-wider">
